f060b43880a9a88761e9c49e04ff5916fd5ff583
Leon Peck <leonpeck234@gmail.com>
2014-11-15 21:46:40 -0600

    Adding decimal tests + functionality to the calc

--- main.js
+++ main.js
@@ -17,20 +17,39 @@ var previousResult;
 var nextOperation;
 
 function add(a, b){
-  return ((a * 100000000000000) + (b * 100000000000000))/100000000000000
-}
+  mantissaofA = a.toString().match(/[^.]*\.?(\d*)/)[1].length;
+  mantissaofB = b.toString().match(/[^.]*\.?(\d*)/)[1].length;
+  var digits = Math.max(mantissaofA, mantissaofB);
+  var sum = a + b
+  return sum.toFixed(digits);
+};
 
 function subtraction(a, b){
-  return ((a * 100000000000000) - (b * 100000000000000))/100000000000000
-}
+mantissaofA = a.toString().match(/[^.]*\.?(\d*)/)[1].length;
+mantissaofB = b.toString().match(/[^.]*\.?(\d*)/)[1].length;
+var digits = Math.max(mantissaofA, mantissaofB);
+var difference = a - b
+return difference.toFixed(digits);
+};
 
 function multiply(a, b){
   return a * b;
-}
+};
 
 function divide(a, b){
-  return (a)/(b);
-}
+  var periodA = a.toString().indexOf(".");
+  var periodB = b.toString().indexOf(".");
+  if (periodA !== -1 || periodB !== -1){
+    mantissaofA = a.toString().match(/[^.]*\.?(\d*)/)[1].length;
+    mantissaofB = b.toString().match(/[^.]*\.?(\d*)/)[1].length;
+    var digits = Math.max(mantissaofA, mantissaofB);
+    var quotient = a / b;
+    return quotient.toFixed(digits);
+  } else {
+    var quotient = a / b;
+    return quotient;
+  }
+};
 
 function currentValue(string){
   return $('#displayoutput').val() * 1;
@@ -87,36 +106,6 @@ function press(buttonValue){
     default:
       var current = $('#displayoutput').val();
       $('#displayoutput').val(current + buttonValue);
-          
+
   }
 }
-// FLOATING POINT IMPRECISION
-  // Any precision operation needs to be performed BEFORE the result is displayed
-    // If not, we'll be correcting AFTER the fact, rather than preventing imprecision.
-    // I think doing so BEFORE will be easier.  Too many variables when it comes to "after".
-// Options
-  // Convert to a string and manipulate (before)
-  // Do some kind of toPrecision and then toFixed or parseFloat (after)
-  // Multiply and divide everything by an outrageous number
-  // split the float value into multiple integer parts.
-
-// A good idea
-//For example, to add 48.6 and 589.55, convert each number to 2 integers:
-//
-//46.80 -> 46 & 80 
-//589.55 -> 589 & 55
-//
-//Add the integers: 
-//46 + 589 = 635 
-//80 + 55 = 135
-//
-//Integer Divide the decimal part by 100: 
-//135 / 100 = 1 
-//135 % 100 = 35
-//
-//635 + 1 = 636
-//
-//Tie on the remainder: 
-//636.35
-//
-//This would require some conversion to string, and doing integer math and then converting back to string. But would yield more accurate results.
